63107b160a9b2ab61eeda02cf479281c6b4de6ab
rajat14 <rajatagarwal.74@gmail.com>
2025-09-08 19:23:32 +0530

    Create index.

---
+++ index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "@auth/AuthContextProvider";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+reportWebVitals();
